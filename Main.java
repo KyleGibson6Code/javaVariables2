d925847549fc9115bfbaa2935c4138e5cf248b86
KyleGibson6Code <kylegibson3gs@gmail.com>
2022-09-15 18:20:41 -0400

    Class Check Out

---
+++ Main.java
@@ -0,0 +1,23 @@
+package javaVariables;
+
+public class Main {
+
+public static void main(String [] args) {
+System.out.println("Kyle Gibson") ;
+	boolean water= true;
+			byte range 20
+			short temperature= 200;
+				int range=25;
+			long range= -3344112211L ;
+			double number= -33.3;
+			float number= -32.f;
+			char letter= '\a3205';
+			String oneWord= "Sky is Blue" ;
+			
+			
+			
+			
+	
+			
+}
+}
